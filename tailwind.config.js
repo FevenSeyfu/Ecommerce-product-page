0c20e16843c40a90c3696d0c59fdc2acad6febce
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-18 12:16:02 +0300

    setup Project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:
+      {
+        //Primary
+        'primary-orange': 'hsl(26, 100%, 55%)',
+        'primary-pale-orange': 'hsl(25, 100%, 94%)',
+        // neutral
+        'neutral-dark blue': 'hsl(220, 13%, 13%)',
+        'neutral-darkGrayish-blue': 'hsl(219, 9%, 45%)',
+        'neutral-bright-blue': 'hsl(220, 14%, 75%)',
+        'neutral-bright-red': 'hsl(223, 64%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+}
