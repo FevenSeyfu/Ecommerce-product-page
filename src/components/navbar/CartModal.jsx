847add8b5427625cca3dafc0626de0d80ac8e8e4
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-25 15:48:59 +0300

    complete cart modal

--- src/components/navbar/CartModal.jsx
+++ src/components/navbar/CartModal.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 const CartModal = ({ cart }) => {
   return (
-    <div className="absolute top-[5rem] mx-2 rounded-2xl shadow-2xl shadow-neutral-700  w-[95vw] md:right-4 md:w-[25rem] bg-white h-[13rem] z-10">
+    <div className="absolute top-[5rem] md:top-[4rem] mx-2 rounded-2xl shadow-2xl shadow-neutral-700  w-[95vw] md:right-0 md:w-[25rem] bg-white h-[14rem] z-10">
       <h1 className="h-16 p-4 font-extrabold text-2xl text-black">Cart</h1>
       <hr className="w-full shadow-md text-gray-900 " />
       <div className="flex flex-col items-center ">
@@ -13,8 +14,8 @@ const CartModal = ({ cart }) => {
         ) : (
           <div className="m-4">
             {cart.map((item) => (
-              <ul className="flex flex-col" key={item.id}>
-                <li className="flex flex-row justify-between gap-4">
+              <ul className="flex flex-col w-full" key={item.id}>
+                <li className="flex flex-row justify-between items-center gap-4 text-gray-500 text-md">
                   <img
                     src={item.img}
                     alt="product image"
@@ -23,17 +24,19 @@ const CartModal = ({ cart }) => {
                   <div className="flex flex-col">
                     <p>{item.name}</p>
                     <p>
-                      ${item.price}x{item.quantity}{" "}
-                      <span className="font-bold">
-                        ${item.price * item.quantity}
+                      ${item.price.toFixed(2) } x {item.quantity}{" "}
+                      <span className="font-bold text-black">
+                        ${(item.price * item.quantity).toFixed(2)}
                       </span>
                     </p>
-                    <button></button>
                   </div>
+                  <button>
+                      <FaRegTrashCan className="text-gray-400 text-xl hover:text-gray-800"/>
+                    </button>
                 </li>
               </ul>
             ))}
-            <button>Checkout</button>
+            <button className="w-full p-2 bg-primary-orange text-white text-xl font-bold rounded-lg mt-4 hover:bg-primary-pale-orange">Checkout</button>
           </div>
         )}
       </div>
