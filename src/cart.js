760388d4555107f3b6793e848e42146d862d7983
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-26 19:51:55 +0300

    Fix typo error in cart array

--- src/cart.js
+++ src/cart.js
@@ -1,7 +1,7 @@
-import product1 from "./assets/image-product-1.jpg",
-import product2 from "./assets/image-product-2.jpg",
-import product3 from "./assets/image-product-3.jpg",
-import product4 from "./assets/image-product-4.jpg",
+import product1 from "./assets/image-product-1.jpg"
+import product2 from "./assets/image-product-2.jpg"
+import product3 from "./assets/image-product-3.jpg"
+import product4 from "./assets/image-product-4.jpg"
 const cart = [
   {
     id: 1,
