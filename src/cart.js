c119b246ddefe8cee525ec0aa7644eaf87c1ba11
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-26 19:50:02 +0300

    update carousle product image inks

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,7 @@
+import product1 from "./assets/image-product-1.jpg",
+import product2 from "./assets/image-product-2.jpg",
+import product3 from "./assets/image-product-3.jpg",
+import product4 from "./assets/image-product-4.jpg",
 const cart = [
   {
     id: 1,
@@ -6,10 +10,8 @@ const cart = [
     description:
       "These low-profile sneakers are your perfect casual wear companion. Featuring a  durable rubber outer sole, they'll withstand everything the weather can offer.",
     images: [
-      "src/assets/image-product-1.jpg",
-      "src/assets/image-product-2.jpg",
-      "src/assets/image-product-3.jpg",
-      "src/assets/image-product-4.jpg",
+      product1,product2,product3,product4
+      
     ],
     price: 125.0,
     quantity: 3,
